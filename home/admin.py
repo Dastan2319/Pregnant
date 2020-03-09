cb5b8872fa67fc20b291cb93ecd2aba158c8a934
Dastan <dastan-4@mail.ru>
2020-03-09 21:03:46 +0600

    first commit

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.Topic)
+admin.site.register(models.Feedback)
+admin.site.register(models.Comments)
+admin.site.register(models.Preparation)
+admin.site.register(models.User)
+admin.site.register(models.NeededItems)
+admin.site.register(models.News)
+
--- home/models.py
+++ home/models.py
@@ -1,3 +1,40 @@
 from django.db import models
-
+from django.db.models import fields as f
 # Create your models here.
+
+class News(models.Model):
+    name = f.CharField(max_length=100 ,null=False)
+    text = f.TextField(null=False)
+    img = f.TextField()
+    tags = f.TextField()
+    date_add = f.DateTimeField(auto_now_add=True)
+
+class Topic(models.Model):
+    title = f.TextField(null=False)
+    text = f.TextField(null=False)
+    date_add = f.DateTimeField(auto_now_add=True)
+
+class User(models.Model):
+    name = f.CharField(max_length=100)
+    password = f.CharField(max_length=20)
+    isActive = True
+    date_add = f.DateTimeField(auto_now_add=True)
+
+class Comments(models.Model):
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    topic=models.ForeignKey(Topic,on_delete = models.CASCADE)
+
+class Preparation(models.Model):
+    title = f.CharField(max_length=100)
+    description = f.TextField()
+
+class NeededItems(models.Model):
+    prepartion = models.ForeignKey(Preparation , on_delete=models.CASCADE)
+    item = f.CharField(max_length=100)
+    quantity = f.IntegerField()
+    recommendationAddress = f.CharField(max_length=100) #типо рекламы
+
+class Feedback(models.Model):
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    text = models.TextField(null=False)
+
