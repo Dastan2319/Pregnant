ebcea861b8658e851f30aad68e525596abf5ceae
Dastan <dastan-4@mail.ru>
2020-03-11 16:15:10 +0600

    add CRUD for news

--- home/views.py
+++ home/views.py
@@ -1,9 +1,42 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views.generic import ListView
+
 from .models import News
+
+
 # Create your views here.
 
 def index(request):
-    d = {'foo':'test'}
+    # d = News.objects.getall()
 
-    return render(request,'index.html',context=d)
+    return render(request, 'index.html')
+
+
+def showNews(request, id: int):
+    addNews = News.objects.get(id=id)
+    return render(request, 'show.html', context=addNews)
+
+
+def CreateNews(request, text: str):
+    name, text, img, tags = text.split(':')
+    addNews = News(name=name, text=text, img=img, tags=tags)
+    addNews.save()
+    return redirect(index)
+
+
+def UpdateNews(request, id: int, text: str):
+    name, text, img, tags = text.split(':')
+    addNews = News.objects.get(id=id)
+    addNews.name = name
+    addNews.text = text
+    addNews.img = img
+    addNews.tags = tags
+    addNews.save()
+    return redirect(index)
+
+
+def DeleteNews(request, id: int):
+    delNews = News.objects.filter(id=id).delete()
+    return redirect(index)
